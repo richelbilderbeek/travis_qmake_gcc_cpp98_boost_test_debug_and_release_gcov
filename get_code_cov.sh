a61a4ef1fc9c16bdc2ce66e796c47ec253ccc7f7
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-10 22:25:57 +0200

    Make get_code_cov.sh less verbose

--- get_code_cov.sh
+++ get_code_cov.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 for filename in `find . | egrep '\.cpp'`; 
 do 
-  gcov -n -o debug $filename > /dev/null; 
-  gcov -n -o release $filename > /dev/null; 
+  gcov -n -o debug $filename
 done
